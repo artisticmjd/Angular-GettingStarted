a86985533ab0369ae490d10e52c69ad9131e45e5
Michael Delgadillo <artisticmjd@gmail.com>
2021-05-20 23:53:53 -0400

    update

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
@@ -7,13 +8,15 @@ import { ProductService } from './product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
   filteredProducts: IProduct[] = [];
   products: IProduct[] = [];
+  errorMessage = '';
+  sub!: Subscription;
 
   private _listFilter = '';
   get listFilter(): string {
@@ -37,11 +40,21 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+    
   }
 
   onRatingClicked(message: string): void{
     this.pageTitle = 'Product List: ' + message;
+  }
+  
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
