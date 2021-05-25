05069863dc76689d0b29de3a46eb7f21e094160f
Michael Delgadillo <artisticmjd@gmail.com>
2021-05-25 01:05:25 -0400

    update

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,28 +1,39 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { IProduct } from "./product";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { IProduct } from './product';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
-
 export class ProductService {
-	private productUrl = 'api/products/products.json';
+  // If using Stackblitz, replace the url with this line
+  // because Stackblitz can't find the api folder.
+  // private productUrl = 'assets/products/products.json';
+  private productUrl = 'api/products/products.json';
 
-	constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) { }
 
-	}
-	
-	getProducts(): Observable<IProduct[]> {
-		return this.http.get<IProduct[]>(this.productUrl)
-      .pipe(tap(data => console.log('All: ', JSON.stringify(data))),
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl)
+      .pipe(
+        tap(data => console.log('All: ', JSON.stringify(data))),
         catchError(this.handleError)
       );
-	}
+  }
 
-	private handleError(err: HttpErrorResponse): Observable<never> {
+  // Get one product
+  // Since we are working with a json file, we can only retrieve all products
+  // So retrieve all products and then find the one we want using 'map'
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts()
+      .pipe(
+        map((products: IProduct[]) => products.find(p => p.productId === id))
+      );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
     // instead of just logging it to the console
     let errorMessage = '';
@@ -37,4 +48,5 @@ export class ProductService {
     console.error(errorMessage);
     return throwError(errorMessage);
   }
+
 }
